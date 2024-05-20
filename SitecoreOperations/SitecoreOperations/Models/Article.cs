df85dd5cd1eaaebcb11a3ed9c81596f48dc915d6
parvathisomanahallicnx <parvathi.somanahalli@concentrix.com>
2024-05-20 14:02:47 +0530

    Article Item Creation
    Integration with Gen AI application , Create article Item in XM cloud using Graph QL

---
+++ SitecoreOperations/SitecoreOperations/Models/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SitecoreOperations.Models
+{
+    public class Article
+    {
+        public string ArticleTitle { get; set; }
+        public string ArticleBody { get; set; }
+        public string ArticleHeadline { get; set; }
+        public string ArticleLeadParagraph { get; set; }
+        public string ArticleExplanation { get; set; }
+    }
+}
--- SitecoreOperations/SitecoreOperations/SitecoreGraphQLOperations/GraphQLOperations.cs
+++ SitecoreOperations/SitecoreOperations/SitecoreGraphQLOperations/GraphQLOperations.cs
@@ -12,47 +12,176 @@ using System.Configuration;
 using SitecoreOperations.Models;
 using GraphQL;
 using System.Threading;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Net.Http;
 
 namespace SitecoreOperations.SitecoreGraphQLOperations
 {
     public class GraphQLOperations
     {
-        //    string GraphQLUrl = ConfigurationManager.AppSettings["graphQLUrl"];
-        //    string AccessToken = ConfigurationManager.AppSettings["accessToken"];
-        //    string PreviewGraphQLUrl = ConfigurationManager.AppSettings["previewGraphQLUrl"];
-        //    string PreviewAccessToken = ConfigurationManager.AppSettings["previewAccessToken"];
-        //    public async Task GetItemFields()
-        //    {
-        //        var graphQLClient = new GraphQLHttpClient(new GraphQLHttpClientOptions
-        //        {
-        //            EndPoint = new Uri(PreviewGraphQLUrl)
-        //        }, new NewtonsoftJsonSerializer());
+        string GraphQLUrl = ConfigurationManager.AppSettings["graphQLUrl"];
+        string AccessToken = ConfigurationManager.AppSettings["accessToken"];
+        string PreviewGraphQLUrl = ConfigurationManager.AppSettings["previewGraphQLUrl"];
+        string PreviewAccessToken = ConfigurationManager.AppSettings["previewAccessToken"];
+        string AuthoringGraphQLUrl = ConfigurationManager.AppSettings["authoringGraphQLUrl"];
+        string AuthoringAccessToken = ConfigurationManager.AppSettings["authoringAccessToken"];
+        string aricleblogGenAI = ConfigurationManager.AppSettings["articleblogGenAI"];
+        public async Task GetItemFields()
+        {
+            var graphQLClient = new GraphQLHttpClient(new GraphQLHttpClientOptions
+            {
+                EndPoint = new Uri(PreviewGraphQLUrl)
+            }, new NewtonsoftJsonSerializer());
 
-        //        //PreviewGraphQLUrl, new NewtonsoftJsonSerializer());
+            //PreviewGraphQLUrl, new NewtonsoftJsonSerializer());
+            graphQLClient.HttpClient.DefaultRequestHeaders.Add("sc_apikey", PreviewAccessToken);
+            string ItemId = "7B486E43327544619799F2A897CC404B";
+            string query = @"query getFields($id:String, $language: String!){
+                                 SitecoreItem:item(path: $id, language: $language) {
+                                     name
+                                    ItemFields:fields{
+                                           name,
+                                           value
+                                            } }}";
+                var request = new GraphQLRequest
+                {
+                    Query = query,
+                    Variables = new { id = ItemId, language = "en" }
+                };
+            var graphQLResponse = await graphQLClient.SendQueryAsync<object>(request);
+            Console.WriteLine(graphQLResponse.Data);
+        }
 
-        //        graphQLClient.HttpClient.DefaultRequestHeaders.Add("sc_apikey", PreviewAccessToken);
-        //        string ItemId = "7B486E43327544619799F2A897CC404B";
-        //        string query = @"query getFields($id:String!){
-        //                         sitecoreitem(path: $id, language: "en") {
-        //                             id
-        //                             name
-        //                            fields{
-        //                            id
-        //                            name
-        //                            }
-        //                             ";
-        //        var request = new GraphQLRequest
-        //        {
-        //            Query = query,
-        //            Variables = new { id = ItemId, language = "en" }
-        //        };
-        //        var graphQLResponse = await graphQLClient.SendQueryAsync<SitecoreItem>(request);
-        //        Console.WriteLine(graphQLResponse.Data);
-        //    }
-        //}
-        public  async Task GetSitecoreItem(CancellationToken cancellationToken, string itemPath)
+        // public async Task CreateArticleItem(String ParentItem, String ItemName, String Query, String Template)
+        public async Task CreateArticleItem(string ParentItem, string ItemName, string Query, string Template)
+        {
+            var graphQLClient = new GraphQLHttpClient(new GraphQLHttpClientOptions
+            {
+                EndPoint = new Uri(AuthoringGraphQLUrl)
+            }, new NewtonsoftJsonSerializer());
+            graphQLClient.HttpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + AuthoringAccessToken);
+
+            string articleTemplate = Constants.Templates.ArticleTemplateId;
+
+            #region Gen AI Call
+
+            var genAIBotApiUrl = @"" + aricleblogGenAI + Query + "/article/" + Template;
+            string resultContentString = string.Empty;
+            using (var client = new HttpClient())
+            {
+                var result = await client.GetAsync(genAIBotApiUrl);
+                resultContentString = await result.Content.ReadAsStringAsync();
+            }
+            var articleItemAI = new Article();
+            if (Template == "template1")
+            {
+                articleItemAI.ArticleTitle = Regex.Split(resultContentString, "ArticleBody--")[0];
+                articleItemAI.ArticleBody = Regex.Split(resultContentString, "ArticleBody--")[1];
+            }
+            else if (Template == "template2")
+            {
+                var articleLeadParagraphSplit = Regex.Split(resultContentString, "ArticleLeadParagraph--");
+                var articleExplanationSplit = Regex.Split(articleLeadParagraphSplit[1], "ArticleExplanation--");
+                articleItemAI.ArticleHeadline = articleLeadParagraphSplit[0];
+                articleItemAI.ArticleLeadParagraph = articleExplanationSplit[0];
+                articleItemAI.ArticleExplanation = articleExplanationSplit[1];
+            }
+
+            #endregion Gen AI Call
+
+            string query = @"mutation($templateId:ID!, $language: String!, $itemName:String!, $parent:ID!,
+                                  $articleTitle: String!, $articleBody: String!, $articleHeadline: String!, $articleLeadParagraph: String!,
+                                   $articleExplanation: String!)
+                                  {
+                                    createItem(
+                                            input: {
+                                                name: $itemName,
+                                                templateId: $templateId ,
+                                                parent: $parent,
+                                                language: $language,
+                                               fields: [{ name: ""ArticleTitle"", value: $articleTitle },
+                                                { name: ""ArticleBody"", value: $articleBody },
+                                                { name: ""ArticleHeadline"", value: $articleHeadline },
+                                                { name: ""ArticleLeadParagraph"", value: $articleLeadParagraph },
+                                                { name: ""ArticleExplanation"", value: $articleExplanation },
+                                                ]
+                                                }
+                                                ) {
+                                                item {
+                                                    itemId
+                                                }
+                                                }
+                                                }";
+            var request = new GraphQLRequest
+            {
+                Query = query,
+                Variables = new { templateId = articleTemplate, itemName = ItemName, language = "en", parent = ParentItem, 
+                    articleTitle = articleItemAI.ArticleTitle ?? string.Empty,
+                    articleBody = articleItemAI.ArticleBody ?? string.Empty,
+                    articleHeadline = articleItemAI.ArticleHeadline ?? string.Empty,
+                    articleLeadParagraph = articleItemAI.ArticleLeadParagraph ?? string.Empty,
+                    articleExplanation = articleItemAI.ArticleExplanation ?? string.Empty
+                }
+            };
+            var graphQLResponse = await graphQLClient.SendMutationAsync<object>(request);
+            Console.WriteLine(graphQLResponse.Data);
+        }
+
+
+        // public async Task CreateBlogItem(String ParentItem, String ItemName, String Query, String Template)
+        public async Task CreateBlogItem()
+        {
+            var graphQLClient = new GraphQLHttpClient(new GraphQLHttpClientOptions
+            {
+                EndPoint = new Uri(AuthoringGraphQLUrl)
+            }, new NewtonsoftJsonSerializer());
+
+            string itemName = "Blog1";
+            string articleTemplate = Constants.Templates.BlogTemplateId;
+            string parentItem = "{14FEBEFA-3EAF-474B-83E5-3C36E41E794D}";
+
+            //GenAI Call
+
+
+
+
+            graphQLClient.HttpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + AuthoringAccessToken);
+
+            string query = @"mutation($templateId:ID!, $language: String!, $itemName:String!, $parent:ID!)
+                                  {
+                                    createItem(
+                                            input: {
+                                                name: $itemName,
+                                                templateId: $templateId ,
+                                                parent: $parent,
+                                                language: $language,
+                                               fields: [{ name: ""BlogTitle"", value: $itemName },
+                                                 { name: ""BlogBody"", value: $itemName },
+                                                { name: ""BlogHilights"", value: $itemName }
+                                                ]
+                                                }
+                                                ) {
+                                                item {
+                                                    itemId
+                                                }
+                                                }
+                                                }";
+            var request = new GraphQLRequest
+            {
+                Query = query,
+                Variables = new
+                {
+                    templateId = articleTemplate,
+                    itemName = itemName,
+                    language = "en",
+                    parent = parentItem,
+                }
+            };
+            var graphQLResponse = await graphQLClient.SendMutationAsync<object>(request);
+            Console.WriteLine(graphQLResponse.Data);
+        }
+
+        public async Task GetSitecoreItem()
         {
             try
             {
@@ -66,11 +195,10 @@ namespace SitecoreOperations.SitecoreGraphQLOperations
                     HttpMethod.Post,
                     "",
                     apikey,
-                    "query { item(path: \"" + itemPath + "\", language: \"en\") {fields(ownFields: true){name,value}}}",
+                    "query { item(path: \"7B486E43327544619799F2A897CC404B\", language: \"en\") {fields(ownFields: true){name,value}}}",
                     new
                     {
-                    },
-                    cancellationToken);
+                    });
                 //Examine the GraphQL response to see if any errors were encountered
                 if (result.Errors?.Count > 0)
                 {
@@ -95,8 +223,6 @@ namespace SitecoreOperations.SitecoreGraphQLOperations
 
 
         }
-
-
     }
 }
 
